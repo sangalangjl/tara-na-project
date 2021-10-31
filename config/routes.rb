5448abbbbc77aab3a2202d3567c375249a498ea7
John Sangalang <sangalangjl@gmail.com>
2021-10-31 16:21:42 -0400

    Added Rails User Authorizing & Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "/hello", to: "application#hello_world"
-
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  resources :users
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
