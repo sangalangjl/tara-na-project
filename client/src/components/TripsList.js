b80d7166bef678d7f8756ac09635f658f2ea552f
John Sangalang <sangalangjl@gmail.com>
2021-11-15 16:01:27 -0500

    Added More Trip CSS

--- client/src/components/TripsList.js
+++ client/src/components/TripsList.js
@@ -40,74 +40,82 @@ const TripsList = ({ trips, leaveTrip, joinTrip, createTrip, cancelTrip }) => {
     }
 
     return (
-        <div>
-            <h3 onClick={handleOnClickTripForm}>+ Add Trip</h3>
-            {toggleNewTrip ?
-                <div>
-                    <form className="NewTripForm" onSubmit={handleSubmit}>
-                        <label htmlFor="name">Name:</label>
-                        <input
-                            type="text"
-                            name="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <label htmlFor="location">Location:</label>
-                        <input
-                            type="text"
-                            name="location"
-                            value={location}
-                            onChange={(e) => setLocation(e.target.value)}
-                        />
-                        <label htmlFor="start_date">Start Date:</label>
-                        <input
-                            type="date"
-                            name="start_date"
-                            value={start_date}
-                            onChange={(e) => setStartDate(e.target.value)}
-                        />
-                        <label htmlFor="end_date">End Date:</label>
-                        <input
-                            type="date"
-                            name="end_date"
-                            value={end_date}
-                            onChange={(e) => setEndDate(e.target.value)}
-                        />
-                        <label htmlFor="img_url">Image URL:</label>
-                        <input
-                            type="text"
-                            name="img_url"
-                            value={img_url}
-                            onChange={(e) => setImgURL(e.target.value)}
-                        />
-                        <button className="NewTripBtn" type="submit">Submit</button>
-                    </form>
-                    <ImCross onClick={() => setToggleNewTrip(false)} />
-                </div> : null}
-            <div>
-                <h1>Trips</h1>
-            </div>
-                <div className="TripCardContainer">
-                    {trips.map(trip => (
-                        <div key={trip.id} className="EachTripCardContainer">
-                            <Link to={`trips/${trip.id}`}>
-                                <div>
-                                    <div className="TripCardImg">
-                                        <img src={trip.img_url} alt={trip.img_url}/>
-                                    </div>
-                                    <div className="TripDetails">
-                                        <h2 className="TripName">{trip.name}</h2>
-                                        <h3 className="TripLocation">{trip.location}</h3>
-                                        <h4 className="TripDuration">Duration: {trip.start_date} to {trip.end_date}</h4>
-                                        <h6 className="TripUniqueID">Unique ID: {trip.id}</h6>
-                                    </div>
-                                </div>
-                            {/* <div>{leaveOrJoinButton(trip)}</div> */}
-                            </Link> 
-                        </div>
-                    ))}
+        <>
+            <div className="NewTripContainer">
+                <div className="TripsListText">
+                    <h2>Trips</h2>
                 </div>
-        </div>
+                <div className="AddTripFormText">
+                    <h3 onClick={handleOnClickTripForm}>+ New Trip</h3>
+                </div>
+                {toggleNewTrip ?
+                    <div className="NewTripForm">
+                        <div className="NewTripText">
+                            <h1>Add a New Trip</h1>
+                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="name">Name:</label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <label htmlFor="location">Location:</label>
+                            <input
+                                type="text"
+                                name="location"
+                                value={location}
+                                onChange={(e) => setLocation(e.target.value)}
+                            />
+                            <label htmlFor="start_date">Start Date:</label>
+                            <input
+                                type="date"
+                                name="start_date"
+                                value={start_date}
+                                onChange={(e) => setStartDate(e.target.value)}
+                            />
+                            <label htmlFor="end_date">End Date:</label>
+                            <input
+                                type="date"
+                                name="end_date"
+                                value={end_date}
+                                onChange={(e) => setEndDate(e.target.value)}
+                            />
+                            <label htmlFor="img_url">Image URL:</label>
+                            <input
+                                type="text"
+                                name="img_url"
+                                value={img_url}
+                                onChange={(e) => setImgURL(e.target.value)}
+                            />
+                            <button className="NewTripBtn" type="submit">Submit</button>
+                        </form>
+                        <ImCross className="CloseTripFromBtn" onClick={() => setToggleNewTrip(false)} />
+                    </div> 
+                : null}
+            </div>
+            <div className="TripCardContainer">
+                {trips.map(trip => (
+                    <div key={trip.id} className="EachTripCardContainer">
+                        <Link to={`trips/${trip.id}`}>
+                            <div>
+                                <div className="TripCardImg">
+                                    <img src={trip.img_url} alt={trip.img_url}/>
+                                </div>
+                                <div className="TripDetails">
+                                    <h2 className="TripName">{trip.name}</h2>
+                                    <h3 className="TripLocation">{trip.location}</h3>
+                                    <h4 className="TripDuration">Duration: {trip.start_date} to {trip.end_date}</h4>
+                                    <h6 className="TripUniqueID">Unique ID: {trip.id}</h6>
+                                </div>
+                            </div>
+                        {/* <div>{leaveOrJoinButton(trip)}</div> */}
+                        </Link> 
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
 
