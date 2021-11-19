c93af113fd977320b6390f4a6438e1dc70bb3361
John Sangalang <sangalangjl@gmail.com>
2021-11-18 19:49:38 -0500

    Test User Img Opacity in Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts "Start Seeding..."
 
-u1 = User.create(username: "johns", first_name: "John", last_name: "Sangalang", email: "sangalangjl@gmail.com", img_url: "https://cdn.pixabay.com/photo/2019/08/11/18/59/icon-4399701_1280.png", password: "password")
+u1 = User.create(username: "johns", first_name: "John", last_name: "Sangalang", email: "sangalangjl@gmail.com", img_url: "https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png", password: "password", admin: false)
 
 
 puts "Done Seeding..."
